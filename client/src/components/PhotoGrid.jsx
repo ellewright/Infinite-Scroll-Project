8c048588c949a062f9981e959c67ffb6a05c486e
Eleanor Wright <elliewright055@gmail.com>
2024-09-27 12:50:10 -0500

    Retrieve photos from api

--- client/src/components/PhotoGrid.jsx
+++ client/src/components/PhotoGrid.jsx
@@ -1,19 +1,27 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 export default function PhotoGrid() {
+    const [photos, setPhotos] = useState([])
+
+    const PHOTOS_URL = `${import.meta.env.VITE_API_URL}/photos-short-list`
+
+    useEffect(() => {
+        axios.get(PHOTOS_URL)
+            .then(res => {
+                setPhotos(res.data)
+            }).catch(err => {
+                console.error(err)
+            })
+    }, [])
+
     return (
         <>
             <div className="grid">
-                <img src="https://via.placeholder.com/600/92c952" />
-                <img src="https://via.placeholder.com/600/771796" />
-                <img src="https://via.placeholder.com/600/24f355" />
-                <img src="https://via.placeholder.com/600/d32776" />
-                <img src="https://via.placeholder.com/600/f66b97" />
-                <img src="https://via.placeholder.com/600/92c952" />
-                <div className="skeleton">Loading...</div>
-                <div className="skeleton">Loading...</div>
-                <div className="skeleton">Loading...</div>
-                <div className="skeleton">Loading...</div>
-                <div className="skeleton">Loading...</div>
-                <div className="skeleton">Loading...</div>
+                {photos.length > 0 ? photos.map(photo => (
+                    <img key={photo.id} src={photo.url} alt={photo.title} />
+                )) : (<></>)
+                }
             </div>
         </>
     )
