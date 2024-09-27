cfe2f1d1416f344e785f632bc02ae30649a47557
Eleanor Wright <elliewright055@gmail.com>
2024-09-27 09:53:45 -0500

    Replace class attribute instances with className

--- client/src/components/PhotoGrid.jsx
+++ client/src/components/PhotoGrid.jsx
@@ -1,19 +1,19 @@
 export default function PhotoGrid() {
     return (
         <>
-            <div class="grid">
+            <div className="grid">
                 <img src="https://via.placeholder.com/600/92c952" />
                 <img src="https://via.placeholder.com/600/771796" />
                 <img src="https://via.placeholder.com/600/24f355" />
                 <img src="https://via.placeholder.com/600/d32776" />
                 <img src="https://via.placeholder.com/600/f66b97" />
                 <img src="https://via.placeholder.com/600/92c952" />
-                <div class="skeleton">Loading...</div>
-                <div class="skeleton">Loading...</div>
-                <div class="skeleton">Loading...</div>
-                <div class="skeleton">Loading...</div>
-                <div class="skeleton">Loading...</div>
-                <div class="skeleton">Loading...</div>
+                <div className="skeleton">Loading...</div>
+                <div className="skeleton">Loading...</div>
+                <div className="skeleton">Loading...</div>
+                <div className="skeleton">Loading...</div>
+                <div className="skeleton">Loading...</div>
+                <div className="skeleton">Loading...</div>
             </div>
         </>
     )
