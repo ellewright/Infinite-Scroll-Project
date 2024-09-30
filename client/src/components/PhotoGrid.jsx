7ff6536ad4a206af0f11e3167860efd03fa6f333
Eleanor Wright <elliewright055@gmail.com>
2024-09-30 16:34:32 -0500

    Add infinite scrolling behavior

--- client/src/components/PhotoGrid.jsx
+++ client/src/components/PhotoGrid.jsx
@@ -1,28 +1,68 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
+import { useCallback, useEffect, useRef, useState } from "react"
+import { parseLinkHeader } from "../functions/parseLinkHeader"
 
 export default function PhotoGrid() {
     const [photos, setPhotos] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const nextPhotoUrlRef = useRef()
 
-    const PHOTOS_URL = `${import.meta.env.VITE_API_URL}/photos-short-list`
+    const BASE_URL = import.meta.env.VITE_API_URL
+    const LIMIT = 10
+
+    async function fetchPhotos(url, { overwrite = false } = {}) {
+        setIsLoading(true)
+
+        try {
+            const res = await fetch(url)
+            nextPhotoUrlRef.current = parseLinkHeader(res.headers.get("Link")).next
+            const photos = await res.json()
+
+            if (overwrite) {
+                setPhotos(photos)
+            } else {
+                setPhotos(prevPhotos => {
+                    return [...prevPhotos, ...photos]
+                })
+            }
+        } catch (e) {
+            console.error(e)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    const imageRef = useCallback((image) => {
+        if (image == null || nextPhotoUrlRef.current == null) return
+
+        const observer = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting) {
+                fetchPhotos(nextPhotoUrlRef.current)
+                observer.unobserve(image)
+            }
+        })
+
+        observer.observe(image)
+    }, [])
 
     useEffect(() => {
-        axios.get(PHOTOS_URL)
-            .then(res => {
-                setPhotos(res.data)
-            }).catch(err => {
-                console.error(err)
-            })
+        fetchPhotos(`${BASE_URL}/photos?_page=1&_limit=${LIMIT}`, { overwrite: true })
     }, [])
 
     return (
-        <>
-            <div className="grid">
-                {photos.length > 0 ? photos.map(photo => (
-                    <img key={photo.id} src={photo.url} alt={photo.title} />
-                )) : (<></>)
-                }
-            </div>
-        </>
+        <div className="grid">
+            {photos.map((photo, index) => (
+                <img
+                    key={photo.id}
+                    src={photo.url}
+                    alt={photo.title}
+                    ref={index === photos.length - 1 ? imageRef : undefined}
+                />
+            ))}
+            {isLoading && (
+                Array.from({ length: LIMIT }, (_, index) => index).map(n => {
+                    <div key={n} className="skeleton">Loading...</div>
+                })
+            )}
+        </div>
     )
 }
